91f0d78d041514c30acb24b72e15ecf0985716b6
Ethan <ethankelty13@gmail.com>
2026-04-18 17:25:24 -0700

    Create +server.ts

---
+++ src/routes/api/slack/+server.ts
@@ -0,0 +1,11 @@
+import type { RequestHandler } from './$types';
+
+export const OPTIONS: RequestHandler = async () => {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
+};
